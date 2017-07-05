0255282b3fe7a5ef7e25cf2ad32615635420d282
Nguyen Chau Chieu <chauchieunguyen@gmail.com>
2017-07-05 20:17:44 +0700

    second commit

---
+++ Travelsistant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Travelsistant
+//
+//  Created by Nguyen Chau Chieu on 7/5/17.
+//  Copyright © 2017 Nguyen Chau Chieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
